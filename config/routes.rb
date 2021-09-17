ed334ed2688188ffe8d1af1c049a1b6e3994bb01
s4na <appletea.umauma@gmail.com>
2021-09-18 03:15:18 +0900

    ニックネームを登録できるように変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root to: 'books#index'
 
   resources :users, only: [:index]
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
